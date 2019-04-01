c43aaf515ace2374d0cf14c1b337c782c511e79c
tedepstein <ted.epstein@reprezen.com>
2019-04-01 15:43:27 -0400

    Fixes #48 - Adds EPL 2.0 headers to all Java and Xtend source files.

--- json-overlay/src/test/java/com/reprezen/jsonoverlay/ScalarTests.java
+++ json-overlay/src/test/java/com/reprezen/jsonoverlay/ScalarTests.java
@@ -1,3 +1,16 @@
+/*********************************************************************
+*  Copyright (c) 2017 ModelSolv, Inc. and others.
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+ *
+ *  Contributors:
+ *     ModelSolv, Inc. 
+ *     - initial API and implementation and/or initial documentation
+**********************************************************************/
 package com.reprezen.jsonoverlay;
 
 import java.math.BigDecimal;
